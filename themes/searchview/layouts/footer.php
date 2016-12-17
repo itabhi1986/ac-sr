431cc87984753cebcd7cde8707416785461969b2
Abhishek Singh <abhishek.it.1986@gmail.com>
2016-12-17 20:34:55 +0530

    pushed site

---
+++ themes/searchview/layouts/footer.php
@@ -0,0 +1,25 @@
+ <div class="bottom_footer">
+    <div class="container">
+      <div class="row clearfix">
+        <div class="col-md-12 column">
+          <div class="row clearfix">
+            <div class="col-md-12 column">
+              <div class="row clearfix">
+               
+                <div class="col-md-6 column copyright text-left">
+                     <ul id="bottom_footer_menu">
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us </a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                  </ul>
+                </div>
+                  <div class=" col-md-6 column copyright text-right">
+                   <p>Copyright © 2016  Search 2 City. All rights reserved.</p>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
